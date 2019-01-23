fa07c541b5aa191daf529c4144366fedcf6c0275
randomgh <31423753+randomgh@users.noreply.github.com>
2019-01-24 01:24:02 +0300

    Bootstrap add

--- NikitaMudriy/src/index.jsx
+++ NikitaMudriy/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from 'app';
-import './index.scss';
+
+import 'bootstrap/scss/bootstrap.scss';
 
 ReactDOM.render(<App/>, document.getElementById('app'));
