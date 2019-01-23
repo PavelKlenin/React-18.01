f91fa1394c944c95788c0b5a9ac819615abecff6
Headmaster11 <headmaster11@yandex.ru>
2019-01-23 22:53:37 +0300

    some update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 let babel_workout = () => {
     [1, 2, 3].map((n) => n + 1);
     console.log('This is how I test babel')
-}
+};
 
-import Developer from './developer'
+import {Developer} from './developer';
 
 const Alex = new Developer('Alex', 26);
+console.log(Alex.age);
