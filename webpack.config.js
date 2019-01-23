c9304029d1e908b7259c03c099fa9c4b3fd21cd9
headmaster11 <headmaster11@yandex.ru>
2019-01-23 13:46:46 +0300

    add loaders and trying to import

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,40 @@ module.exports = {
     output: {
       filename: 'main.js',
       path: path.resolve(__dirname, 'dist')
+    },
+    module:{
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(html)$/,
+          use: {
+            loader: 'html-loader',
+            options: {
+              attrs: [':data-src']
+            }
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+              "style-loader", // creates style nodes from JS strings
+              "css-loader", // translates CSS into CommonJS
+              "sass-loader" // compiles Sass to CSS, using Node Sass by default
+          ]
+        },        
+      ]
     }
   };
