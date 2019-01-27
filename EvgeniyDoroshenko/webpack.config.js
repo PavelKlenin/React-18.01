4752af541676a30f091e0d9191252acc3e146dcb
kraspy <kraspy@yandex.ru>
2019-01-27 20:50:15 +0700

    GB_React-task3

--- EvgeniyDoroshenko/webpack.config.js
+++ EvgeniyDoroshenko/webpack.config.js
@@ -3,18 +3,31 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 const config = {
-  entry: path.resolve(__dirname, 'src', 'js', 'main.js'),
+  entry: path.resolve(__dirname, 'src', 'js', '_main.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'script.js'
+    path: path.resolve(__dirname, 'app'),
+    filename: 'App.js'
   },
-  plugins: [new HtmlWebpackPlugin()],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'app'),
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'html', 'tmp.html'),
+      title: 'GB_React_hw_3'
+    }
+  )],
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: '/node_modules/',
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader']
       }
     ]
   }
