fafbf1755830bf9f824841db06a3709b8ac7c868
kraspy <kraspy@yandex.ru>
2019-01-28 10:54:22 +0700

    fix

--- EvgeniyDoroshenko/webpack.config.js
+++ EvgeniyDoroshenko/webpack.config.js
@@ -1,33 +1,32 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
-  entry: path.resolve(__dirname, 'src', 'js', '_main.js'),
+  entry: path.resolve(__dirname, "src", "js", "_main.js"),
   output: {
-    path: path.resolve(__dirname, 'app'),
-    filename: 'App.js'
+    path: path.resolve(__dirname, "app"),
+    filename: "App.js"
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'app'),
+    contentBase: path.resolve(__dirname, "app"),
     port: 9000
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'html', 'tmp.html'),
-      title: 'GB_React_hw_3'
-    }
-  )],
+      template: path.resolve(__dirname, "src", "html", "tmp.html"),
+      title: "GB_React_hw_3"
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: '/node_modules/',
-        loader: 'babel-loader'
+        exclude: "/node_modules/",
+        loader: "babel-loader"
       },
       {
         test: /\.css$/,
-        loader: ['style-loader', 'css-loader']
+        loader: ["style-loader", "css-loader"]
       }
     ]
   }
