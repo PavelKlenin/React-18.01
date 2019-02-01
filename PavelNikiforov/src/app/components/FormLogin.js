fb56c12eb682ff8547c60d8af34880b9147147ef
PawelNikiforow <npd264@gmail.com>
2019-02-01 12:47:19 +0300

    Lesson 4

--- PavelNikiforov/src/app/components/FormLogin.js
+++ PavelNikiforov/src/app/components/FormLogin.js
@@ -4,11 +4,11 @@ class FormLogin extends React.Component {
     render() {
         return (
             <form className="form__login">
-                <label htmlFor="" className="form__label">
+                <label className="form__label">
                     <span className="form__label_text">Login:</span>
                     <input type="text" className="form__input"/>
                 </label>
-                <label htmlFor="" className="form__label">
+                <label className="form__label">
                     <span className="form__label_text">Password:</span>
                     <input type="password" className="form__input"/>
                 </label>
