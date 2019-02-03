98811beb4a8cbfb093ce1dfc0b99990ea809452b
kraspy <kraspy@yandex.ru>
2019-02-03 22:25:29 +0700

    ...

---
+++ EvgeniyDoroshenko/src/app/pages/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid at consectetur corporis cum dolorum earum, exercitationem facilis ipsam nostrum obcaecati optio pariatur quod rem reprehenderit. Ab nobis quidem voluptatibus!</p>
+      </div>
+    );
+  }
+}
+
+export default Main;
