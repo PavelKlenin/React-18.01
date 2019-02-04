88f7bfd640eaca7a9d1b0c348461df8c29e7f88b
ampersky86 <gasttello@gmail.com>
2019-02-04 11:25:20 +0300

    Запилины Main About Contacts

---
+++ AleksandrAkperov/hw_5/src/app/components/Contacts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default class  Contacts extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Контакты</h1>
+                <p>email: 1@2.local</p>
+                <p>Telephone: +7(000) 000-00-00</p>
+            </div>
+        )
+    }
+}
---
+++ AleksandrAkperov/hw_5/src/app/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default class  Layout extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Главная страница</h1>
+                <p>Добро пожаловать</p>
+            </div>
+        )
+    }
+}
