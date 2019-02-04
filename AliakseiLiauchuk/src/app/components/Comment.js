23cd9b7d1f7f35113abc4684c3e615a72ca9eea5
headmaster11 <headmaster11@yandex.ru>
2019-02-04 11:36:26 +0300

    finish lesson5 homework

--- AliakseiLiauchuk/src/app/components/Comment.js
+++ AliakseiLiauchuk/src/app/components/Comment.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router-3';
 
-export default class Post extends React.Component {
+export default class Comment extends React.Component {
   render() {
     return (
 
         <div className="card border-secondary mb-3">
           <div className="card-header">
-          <Link to={`/posts/${this.props.id}`}>
+          <Link to={`/comments/${this.props.id}`}>
           {this.props.id}
           </Link>
           </div>
           <div className="card-body text-secondary">
-            <p>{this.props.title}</p>
+            <p>{this.props.name}</p>
+            <p>{this.props.email}</p>
             <p>{this.props.body}</p>
           </div>
         </div>
