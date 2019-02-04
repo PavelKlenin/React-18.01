640c028a41c36c2331df5da8a135bb5a15b146a2
ampersky86 <gasttello@gmail.com>
2019-02-04 09:31:31 +0300

    Добавлены доп. пакеты

---
+++ AleksandrAkperov/hw_5/src/app/layouts/Layout.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import  {Link} from 'react-router';
+
+export default class  Layout extends React.Component {
+    render() {
+        return (
+            <div className="container" >
+                <div className="row">
+                    <div className="col-4">
+                        <ul>
+                            <li><Link to="/">Главная</Link></li>
+                            <li><Link to="/about">О нас</Link></li>
+                            <li><Link to="/contacts">Контакты</Link></li>
+                        </ul>
+                    </div>
+                    <div className="col-8">
+                        {this.props.children}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
