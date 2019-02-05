639b54bf10eae664498c7267aa5f9214b44887e6
Dafnochka <dlushpina@gmail.com>
2019-02-05 15:57:20 +0300

    Блог вар 3

--- DariaGuketleva/blog/src/App.js
+++ DariaGuketleva/blog/src/App.js
@@ -9,6 +9,11 @@ import Main from './app/pages/Main';
 import Users from './app/pages/Users';
 import User from './app/pages/User';
 import PageNotFound from './app/pages/PageNotFound'
+import Posts from './app/pages/Posts'
+import Post from './app/pages/Post'
+
+import Comments from './app/pages/Comments'
+import Comment from './app/pages/Comment'
 
 class App extends React.Component {
 
@@ -17,7 +22,13 @@ class App extends React.Component {
             <Route path='/' component={Layout}>
                 <IndexRoute component={Main}/>
                 <Route path='users' component={Users}>
-                    <Route path=':userID' component={User}/>
+                    <Route path=':userId' component={User}/>
+                </Route>
+                <Route path='posts' component={Posts}>
+                    <Route path=':postId' component={Post}/>
+                </Route>
+                <Route path='comments' component={Comments}>
+                    <Route path=':commentId' component={Comment}/>
                 </Route>
                 <Route path="*" component={PageNotFound}/>
             </Route>
