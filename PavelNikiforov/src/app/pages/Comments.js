b983308193de084624d45ab892bc9ea405094c25
PawelNikiforow <npd264@gmail.com>
2019-02-05 13:30:05 +0300

    Lesson 5, clear logs

--- PavelNikiforov/src/app/pages/Comments.js
+++ PavelNikiforov/src/app/pages/Comments.js
@@ -3,7 +3,6 @@ import CommentList from '../components/CommentList'
 
 class Comments extends React.Component {
     render() {
-        console.log(this)
         return (
             <>
                 {!this.props.children ? <CommentList/> : this.props.children}
