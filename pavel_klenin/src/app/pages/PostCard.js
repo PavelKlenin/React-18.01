4dcb2ff8200ffbab5ae0527f458bdd2eb9c8a56a
P_Klenin <pashka_89@mail.ru>
2019-02-18 09:31:20 +0300

    HW lesson-7 fixed

--- pavel_klenin/src/app/pages/PostCard.js
+++ pavel_klenin/src/app/pages/PostCard.js
@@ -9,31 +9,37 @@ import Post from '../components/Post';
 import style from '../style/PostCard.css';
 
 class PostCard extends React.Component {
-    constructor (props) {
-        super(props);
-    }
-
-    render () {
-        const comments = this.props.comments.map((comment, index) => {
-            return (
-                <Comment key={index} {...comment}/>
-            );
-        })
-        return(
-            <div>
-                <Post {...this.props.post} author ={this.props.author}/>
-                <hr/>
-                <h3>Comments</h3>
-                <hr/>
-                {comments}
-            </div>
-        );
-    }
 
     componentDidMount () {
         const postID = this.props.params.postID;
         this.props.dispatch(fetchPostCard(postID));
     }
+
+    render () {
+        let postCard = 'loading...';
+        if (this.props.post.id === +this.props.params.postID)
+        {
+            const comments = this.props.comments.map((comment, index) => {
+                return (
+                    <Comment key={index} {...comment}/>
+                );
+            })
+
+            postCard = (
+                <div>
+                    <Post {...this.props.post} author ={this.props.author}/>
+                    <hr/>
+                    <h3>Comments</h3>
+                    <hr/>
+                    {comments}
+                </div>
+            )
+        }
+
+        return postCard;
+    }
+
+
 }
 
 const mapStatetoProps = (state) => ({
